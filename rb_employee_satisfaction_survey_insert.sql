2e1f1ee66d4189aa6814b7631f93949a0292ae83
Bhaskaran Rathnachalam <r.bhaskarann@gmail.com>
2025-04-16 10:06:16 -0700

    employee satisfaction comments

---
+++ rb_employee_satisfaction_survey_insert.sql
@@ -0,0 +1,143 @@
+-- Employee Satisfaction Survey Comments
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2000, 'There is a lack of end user because career development was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2001, 'There is a lack of welcome process because manager support was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2002, 'I have concerns about IJP because career development was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2003, 'I wish there was more onboarding but customer was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2004, 'I have concerns about end user because learning was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2005, 'There is a lack of peers but client was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2006, 'I wish there was more internal transfer while welcome process was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2007, 'I had a great experience with lateral move because assistance was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2008, 'I found value in customer and collaboration was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2009, 'I really appreciate learning because welcome process was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2010, 'I have concerns about promotion and team was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2011, 'I had a great experience with coworkers and orientation was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2012, 'I found value in welcome process but from_within was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2013, 'I wish there was more peers while guidance was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2014, 'I really appreciate team and welcome process was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2015, 'I have concerns about collaboration and IJP was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2016, 'I have concerns about team but end user was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2017, 'There is a lack of client but career development was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2018, 'I have concerns about end user because from_within was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2019, 'I found value in promotion but orientation was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2020, 'I have concerns about client and IJP was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2021, 'I have concerns about assistance and from_within was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2022, 'I wish there was more orientation and HR help was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2023, 'There is a lack of orientation because HR help was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2024, 'I found value in learning because internal transfer was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2025, 'I wish there was more learning while lateral move was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2026, 'I have concerns about end user and peers was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2027, 'I wish there was more HR help because peers was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2028, 'I had a great experience with onboarding but learning was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2029, 'I found value in IJP while orientation was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2030, 'I found value in learning but IJP was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2031, 'I had a great experience with from_within because coworkers was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2032, 'I have concerns about end user and internal transfer was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2033, 'I have concerns about assistance because career development was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2034, 'I had a great experience with coworkers while career development was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2035, 'I found value in coworkers but welcome process was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2036, 'I have concerns about career growth but lateral move was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2037, 'I really appreciate career development because customer was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2038, 'I have concerns about onboarding while collaboration was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2039, 'I have concerns about welcome process because customer was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2040, 'I really appreciate from_within but manager support was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2041, 'I found value in career growth because HR help was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2042, 'I wish there was more onboarding while learning was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2043, 'I have concerns about team because learning was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2044, 'I have concerns about from_within while orientation was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2045, 'I really appreciate internal transfer because coworkers was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2046, 'I really appreciate HR help while client was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2047, 'There is a lack of internal transfer but client was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2048, 'I wish there was more HR help and welcome process was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2049, 'There is a lack of promotion but welcome process was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2050, 'I wish there was more onboarding while from_within was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2051, 'I really appreciate welcome process while IJP was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2052, 'I have concerns about collaboration while client was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2053, 'I have concerns about assistance because customer was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2054, 'I found value in from_within and collaboration was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2055, 'I had a great experience with IJP while assistance was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2056, 'I found value in welcome process because career growth was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2057, 'I really appreciate manager support but end user was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2058, 'I really appreciate coworkers because orientation was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2059, 'I have concerns about coworkers and customer was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2060, 'I really appreciate guidance because lateral move was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2061, 'I have concerns about collaboration and end user was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2062, 'I have concerns about client because onboarding was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2063, 'I found value in welcome process because career growth was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2064, 'I had a great experience with end user and from_within was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2065, 'I have concerns about guidance while client was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2066, 'There is a lack of team and promotion was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2067, 'I wish there was more promotion because coworkers was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2068, 'There is a lack of orientation while guidance was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2069, 'I wish there was more client and coworkers was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2070, 'There is a lack of team while end user was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2071, 'I had a great experience with career development because HR help was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2072, 'There is a lack of career development and onboarding was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2073, 'I wish there was more internal transfer while end user was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2074, 'I wish there was more welcome process but HR help was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2075, 'There is a lack of IJP and career growth was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2076, 'I found value in career growth because internal transfer was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2077, 'There is a lack of career development while end user was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2078, 'I found value in customer while promotion was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2079, 'I found value in peers and lateral move was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2080, 'I wish there was more career growth because guidance was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2081, 'I have concerns about career development but onboarding was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2082, 'I found value in HR help while promotion was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2083, 'I found value in collaboration while orientation was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2084, 'I wish there was more client but manager support was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2085, 'I have concerns about end user because team was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2086, 'I found value in learning while orientation was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2087, 'I wish there was more lateral move because guidance was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2088, 'I found value in client and career growth was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2089, 'I really appreciate collaboration because orientation was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2090, 'I have concerns about lateral move and guidance was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2091, 'I found value in coworkers while orientation was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2092, 'I wish there was more customer but career growth was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2093, 'I had a great experience with lateral move and career development was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2094, 'I really appreciate orientation and from_within was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2095, 'I wish there was more onboarding while coworkers was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2096, 'There is a lack of learning because orientation was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2097, 'I have concerns about HR help while lateral move was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2098, 'I wish there was more onboarding but guidance was inconsistent.');
+INSERT INTO rb_survey_comments (respondent_id, comment_text) VALUES (2099, 'There is a lack of collaboration while orientation was inconsistent.');
+
+-- Word Groups
+INSERT INTO rb_word_groups (group_name, words) VALUES ('#bacghr_client', '"client" OR "customer" OR "user" OR "end user"');
+INSERT INTO rb_word_groups (group_name, words) VALUES ('#bacghr_internalmove', '"internal" OR "transfer" OR "from_within" OR "lateral" OR "IJP"');
+INSERT INTO rb_word_groups (group_name, words) VALUES ('#bacghr_career', '"career" OR "promotion" OR "growth" OR "development"');
+INSERT INTO rb_word_groups (group_name, words) VALUES ('#bacghr_onboarding', '"onboarding" OR "orientation" OR "induction"');
+INSERT INTO rb_word_groups (group_name, words) VALUES ('#bacghr_team', '"team" OR "collaboration" OR "cooperation" OR "group"');
+INSERT INTO rb_word_groups (group_name, words) VALUES ('#bacghr_support', '"support" OR "assistance" OR "help" OR "aid"');
+
+-- Comment Rules
+INSERT INTO rb_comment_rules (topic, rule_expr_raw, exclude_expr_raw) VALUES ('Client Satisfaction', '#bacghr_client NEAR #bacghr_support WITHIN 5', NULL);
+INSERT INTO rb_comment_rules (topic, rule_expr_raw, exclude_expr_raw) VALUES ('Internal Mobility', '#bacghr_internalmove NEAR opportunity WITHIN 4', 'attorney OR contractor');
+INSERT INTO rb_comment_rules (topic, rule_expr_raw, exclude_expr_raw) VALUES ('Career Growth', '#bacghr_career NEAR lacking WITHIN 6', NULL);
+INSERT INTO rb_comment_rules (topic, rule_expr_raw, exclude_expr_raw) VALUES ('Onboarding', '#bacghr_onboarding NEAR helpful WITHIN 3', NULL);
+INSERT INTO rb_comment_rules (topic, rule_expr_raw, exclude_expr_raw) VALUES ('Team Culture', '#bacghr_team NEAR issues WITHIN 2', '#bacghr_client');
+
+
+-- Sample Queries
+
+-- Find all comments matching rules
+SELECT c.id, c.comment_text, r.topic
+FROM rb_survey_comments c
+JOIN rb_comment_rules r
+  ON CONTAINS(c.comment_text, expand_macros(r.rule_expr_raw), 1) > 0
+WHERE r.exclude_expr_raw IS NULL 
+   OR CONTAINS(c.comment_text, expand_macros(r.exclude_expr_raw), 2) = 0;
+
+-- Highlighted matched snippet
+SELECT c.id,
+       CTX_DOC.SNIPPET('comment_text_idx', c.id, expand_macros(r.rule_expr_raw)) AS snippet
+FROM rb_survey_comments c
+JOIN rb_comment_rules r
+  ON CONTAINS(c.comment_text, expand_macros(r.rule_expr_raw), 1) > 0;
+
+-- Summary count by topic
+SELECT r.topic, COUNT(*) AS match_count
+FROM rb_survey_comments c
+JOIN rb_comment_rules r
+  ON CONTAINS(c.comment_text, expand_macros(r.rule_expr_raw), 1) > 0
+WHERE r.exclude_expr_raw IS NULL 
+   OR CONTAINS(c.comment_text, expand_macros(r.exclude_expr_raw), 2) = 0
+GROUP BY r.topic;
