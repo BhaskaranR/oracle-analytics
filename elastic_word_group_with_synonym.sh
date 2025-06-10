f5cd706ccd721a60267a58a7cb2441faa1f7150a
Bhaskaran Rathnachalam <r.bhaskarann@gmail.com>
2025-06-10 11:19:25 -0700

    add synonym to comment_rules

--- elastic_word_group_with_synonym.sh
+++ elastic_word_group_with_synonym.sh
@@ -42,24 +42,42 @@ curl -X PUT "localhost:9200/comments_index" -H 'Content-Type: application/json' 
 # Step 2: Create comment_rules index with percolator mapping
 curl -X PUT "localhost:9200/comment_rules" -H 'Content-Type: application/json' -d '
 {
+  "settings": {
+    "analysis": {
+      "filter": {
+        "custom_synonym_filter": {
+          "type": "synonym",
+          "synonyms": [
+            "#bacghr_client => client, customer, user, end user",
+            "#bacghr_internalmove => internal, transfer, from_within, lateral, IJP",
+            "#bacghr_career => career, promotion, growth, development",
+            "#bacghr_onboarding => onboarding, orientation, induction",
+            "#bacghr_team => team, collaboration, cooperation, group",
+            "#bacghr_support => support, assistance, help, aid"
+          ]
+        }
+      },
+      "analyzer": {
+        "custom_synonym_analyzer": {
+          "tokenizer": "standard",
+          "filter": [
+            "lowercase",
+            "custom_synonym_filter"
+          ]
+        }
+      }
+    }
+  },
   "mappings": {
     "properties": {
-      "query": {
-        "type": "percolator"
-      },
-      "topic": {
-        "type": "keyword"
-      },
-      "group_refs": {
-        "type": "keyword"
-      },
-      "text": {
-        "type": "text",
-        "analyzer": "custom_synonym_analyzer"
-      }
+      "query":    { "type": "percolator" },
+      "topic":    { "type": "keyword" },
+      "group_refs": { "type": "keyword" },
+      "text":     { "type": "text", "analyzer": "custom_synonym_analyzer" }
     }
   }
 }'
+
 
 # Step 3: Add comment rules as percolator queries
 curl -X POST "localhost:9200/comment_rules/_bulk" -H 'Content-Type: application/json' -d '
