f2f1246cf1b98c32296677b582a2694aa9926d8e
Bhaskaran Rathnachalam <r.bhaskarann@gmail.com>
2025-06-11 08:57:26 -0700

    remove group_refs

--- elastic_word_group_with_synonym_intervals.sh
+++ elastic_word_group_with_synonym_intervals.sh
@@ -38,34 +38,33 @@ curl -X PUT "localhost:9200/comment_rules" -H 'Content-Type: application/json' -
   "settings": {
     "analysis": {
       "filter": {
-        "synonym_filter": {
+        "synonym_rules": {
           "type": "synonym_graph",
-          "synonyms_set": "word_groups"
+          "synonyms_set": "word_groups",
+          "updateable": true
         }
       },
       "analyzer": {
-        "synonym_analyzer": {
+        "synonym_search_analyzer": {
+          "type": "custom",
           "tokenizer": "standard",
-          "filter": ["lowercase", "synonym_filter"]
+          "filter": ["lowercase", "synonym_rules"]
         }
       }
     }
   },
   "mappings": {
     "properties": {
-      "rule_query": {
+      "query": {
         "type": "percolator"
       },
       "topic": {
         "type": "keyword"
       },
-      "group_refs": {
-        "type": "keyword"
-      },
       "comment_text": {
         "type": "text",
-        "analyzer": "synonym_analyzer",
-        "search_analyzer": "synonym_analyzer"
+        "analyzer": "synonym_search_analyzer",
+        "search_analyzer": "synonym_search_analyzer"
       }
     }
   }
@@ -74,27 +73,27 @@ curl -X PUT "localhost:9200/comment_rules" -H 'Content-Type: application/json' -
 # Step 3: Bulk index percolator rules
 curl -X POST "localhost:9200/comment_rules/_bulk" -H 'Content-Type: application/x-ndjson' -d '
 {"index":{"_id":"1"}}
-{"topic":"Client Support","group_refs":["bacghr_client","bacghr_support"],"rule_query":{"intervals":{"comment_text":{"all_of":{"ordered":false,"intervals":[{"match":{"query":"bacghr_client","analyzer":"synonym_analyzer","max_gaps":5}},{"match":{"query":"bacghr_support","analyzer":"synonym_analyzer","max_gaps":5}}]}}}}}
+{"topic":"Client Support","query":{"intervals":{"comment_text":{"all_of":{"ordered":false,"intervals":[{"match":{"query":"bacghr_client","analyzer":"synonym_analyzer","max_gaps":5}},{"match":{"query":"bacghr_support","analyzer":"synonym_analyzer","max_gaps":5}}]}}}}}
 {"index":{"_id":"2"}}
-{"topic":"Career Concerns","group_refs":["bacghr_career"],"rule_query":{"intervals":{"comment_text":{"all_of":{"ordered":false,"intervals":[{"match":{"query":"lack","analyzer":"synonym_analyzer","max_gaps":2}},{"match":{"query":"bacghr_career","analyzer":"synonym_analyzer","max_gaps":2}}]}}}}}
+{"topic":"Career Concerns","query":{"intervals":{"comment_text":{"all_of":{"ordered":false,"intervals":[{"match":{"query":"lack","analyzer":"synonym_analyzer","max_gaps":2}},{"match":{"query":"bacghr_career","analyzer":"synonym_analyzer","max_gaps":2}}]}}}}}
 {"index":{"_id":"3"}}
-{"topic":"Client Satisfaction","rule_query":{"intervals":{"comment_text":{"all_of":{"ordered":false,"intervals":[{"match":{"query":"client","analyzer":"synonym_analyzer","max_gaps":5}},{"match":{"query":"support","analyzer":"synonym_analyzer","max_gaps":5}}]}}}}}
+{"topic":"Client Satisfaction","query":{"intervals":{"comment_text":{"all_of":{"ordered":false,"intervals":[{"match":{"query":"client","analyzer":"synonym_analyzer","max_gaps":5}},{"match":{"query":"support","analyzer":"synonym_analyzer","max_gaps":5}}]}}}}}
 {"index":{"_id":"4"}}
-{"topic":"Learning and Growth","rule_query":{"intervals":{"comment_text":{"all_of":{"ordered":false,"intervals":[{"match":{"query":"learning","analyzer":"synonym_analyzer","max_gaps":3}},{"match":{"query":"growth","analyzer":"synonym_analyzer","max_gaps":3}}]}}}}}
+{"topic":"Learning and Growth","query":{"intervals":{"comment_text":{"all_of":{"ordered":false,"intervals":[{"match":{"query":"learning","analyzer":"synonym_analyzer","max_gaps":3}},{"match":{"query":"growth","analyzer":"synonym_analyzer","max_gaps":3}}]}}}}}
 {"index":{"_id":"5"}}
-{"topic":"Team Collaboration","rule_query":{"intervals":{"comment_text":{"all_of":{"ordered":false,"intervals":[{"match":{"query":"team","analyzer":"synonym_analyzer","max_gaps":4}},{"match":{"query":"collaboration","analyzer":"synonym_analyzer","max_gaps":4}}]}}}}}
+{"topic":"Team Collaboration","query":{"intervals":{"comment_text":{"all_of":{"ordered":false,"intervals":[{"match":{"query":"team","analyzer":"synonym_analyzer","max_gaps":4}},{"match":{"query":"collaboration","analyzer":"synonym_analyzer","max_gaps":4}}]}}}}}
 {"index":{"_id":"6"}}
-{"topic":"Career Progression","rule_query":{"intervals":{"comment_text":{"all_of":{"ordered":false,"intervals":[{"match":{"query":"promotion","analyzer":"synonym_analyzer","max_gaps":4}},{"match":{"query":"career","analyzer":"synonym_analyzer","max_gaps":4}}]}}}}}
+{"topic":"Career Progression","query":{"intervals":{"comment_text":{"all_of":{"ordered":false,"intervals":[{"match":{"query":"promotion","analyzer":"synonym_analyzer","max_gaps":4}},{"match":{"query":"career","analyzer":"synonym_analyzer","max_gaps":4}}]}}}}}
 {"index":{"_id":"7"}}
-{"topic":"Client Help","rule_query":{"intervals":{"comment_text":{"all_of":{"ordered":false,"intervals":[{"match":{"query":"bacghr_client","analyzer":"synonym_analyzer","max_gaps":4}},{"match":{"query":"bacghr_support","analyzer":"synonym_analyzer","max_gaps":4}}]}}}}}
+{"topic":"Client Help","query":{"intervals":{"comment_text":{"all_of":{"ordered":false,"intervals":[{"match":{"query":"bacghr_client","analyzer":"synonym_analyzer","max_gaps":4}},{"match":{"query":"bacghr_support","analyzer":"synonym_analyzer","max_gaps":4}}]}}}}}
 {"index":{"_id":"8"}}
-{"topic":"Onboarding Feedback","rule_query":{"intervals":{"comment_text":{"match":{"query":"orientation experience","analyzer":"synonym_analyzer","ordered":true,"max_gaps":2}}}}}
+{"topic":"Onboarding Feedback","query":{"intervals":{"comment_text":{"match":{"query":"orientation experience","analyzer":"synonym_analyzer","ordered":true,"max_gaps":2}}}}}
 {"index":{"_id":"9"}}
-{"topic":"Team Support","rule_query":{"intervals":{"comment_text":{"all_of":{"ordered":false,"intervals":[{"match":{"query":"team","analyzer":"synonym_analyzer","max_gaps":3}},{"match":{"query":"help","analyzer":"synonym_analyzer","max_gaps":3}}]}}}}}
+{"topic":"Team Support","query":{"intervals":{"comment_text":{"all_of":{"ordered":false,"intervals":[{"match":{"query":"team","analyzer":"synonym_analyzer","max_gaps":3}},{"match":{"query":"help","analyzer":"synonym_analyzer","max_gaps":3}}]}}}}}
 {"index":{"_id":"10"}}
-{"topic":"Internal Movement","rule_query":{"intervals":{"comment_text":{"all_of":{"ordered":false,"intervals":[{"match":{"query":"internal","analyzer":"synonym_analyzer","max_gaps":3}},{"match":{"query":"opportunity","analyzer":"synonym_analyzer","max_gaps":3}}]}}}}}
+{"topic":"Internal Movement","query":{"intervals":{"comment_text":{"all_of":{"ordered":false,"intervals":[{"match":{"query":"internal","analyzer":"synonym_analyzer","max_gaps":3}},{"match":{"query":"opportunity","analyzer":"synonym_analyzer","max_gaps":3}}]}}}}}
 {"index":{"_id":"11"}}
-{"topic":"Employee Training","rule_query":{"intervals":{"comment_text":{"all_of":{"ordered":false,"intervals":[{"match":{"query":"training","analyzer":"synonym_analyzer","max_gaps":3}},{"match":{"query":"session","analyzer":"synonym_analyzer","max_gaps":3}}]}}}}}
+{"topic":"Employee Training","query":{"intervals":{"comment_text":{"all_of":{"ordered":false,"intervals":[{"match":{"query":"training","analyzer":"synonym_analyzer","max_gaps":3}},{"match":{"query":"session","analyzer":"synonym_analyzer","max_gaps":3}}]}}}}}
 {"index":{"_id":"12"}}
-{"topic":"Onboarding Process","rule_query":{"intervals":{"comment_text":{"all_of":{"ordered":false,"intervals":[{"match":{"query":"onboarding","analyzer":"synonym_analyzer","max_gaps":2}},{"match":{"query":"process","analyzer":"synonym_analyzer","max_gaps":2}}]}}}}}
+{"topic":"Onboarding Process","query":{"intervals":{"comment_text":{"all_of":{"ordered":false,"intervals":[{"match":{"query":"onboarding","analyzer":"synonym_analyzer","max_gaps":2}},{"match":{"query":"process","analyzer":"synonym_analyzer","max_gaps":2}}]}}}}}
 '
